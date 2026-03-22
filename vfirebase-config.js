72c3dac04d8cb7b3ce61efbbf67557ef4347506d
Ignacio <sistemas@raitrai.cl>
2026-03-22 14:41:25 -0300

    Create vfirebase-config.js

---
+++ vfirebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdx9nVcV-UiGER3mcz-w9BcSSIZd-t5nE",
+  authDomain: "sist-op-rt.firebaseapp.com",
+  projectId: "sist-op-rt",
+  storageBucket: "sist-op-rt.firebasestorage.app",
+  messagingSenderId: "438607695630",
+  appId: "1:438607695630:web:f5a16f319e3ea17fbfd15f"
+};
